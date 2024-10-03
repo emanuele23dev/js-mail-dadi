99be6bbb4791bc1c142f2a39489f671eaa9bc0f5
emanuele23dev <emanuele.meggiarin@libero.it>
2024-10-03 18:49:12 +0200

    end mail

--- mail/index.js
+++ mail/index.js
@@ -16,10 +16,11 @@ if (email_list.includes(user_email)) {
   alert("Hai inserito correttamente la tua email");
   console.log(user_email);
   }
-else {
+
+  else {
   alert("Email non corretta, riprova");
   console.log(user_email);
-}
+} 
 
 
 
