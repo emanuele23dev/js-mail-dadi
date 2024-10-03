b5a3aa2ac0215c06feec06be9555a98563ebdbba
emanuele23dev <emanuele.meggiarin@libero.it>
2024-10-03 16:24:53 +0200

    first step

---
+++ dadi/index.js
@@ -0,0 +1,8 @@
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+ 
+let user = parseInt(Math.floor(Math.random() * 6) + 1);
+let computer = parseInt(Math.floor(Math.random() * 6) + 1);
+
+console.log(`Il giocatore ha ottenuto il numero ${user}`);
+console.log(`Il computer ha ottenuto il numero ${computer}`);
+
